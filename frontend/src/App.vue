90a25d4a0b8ecd60c378c83822b07d04d594c259
Ostap-IM <ostap.fedchuk@intramind-srl.com>
2026-03-04 19:43:12 +0100

    Settaggio del frontend base

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,19 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <Header />
+    <Footer />
+    <router-view />
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+  import Header from './components/Header.vue'
+  import Footer from './components/Footer.vue'
+
+</script>
+
+
+
+<style scoped>
+
+</style>
